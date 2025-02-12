c8cc80442d53e9ccbb9091659f4692d675a91446
rahultamkhane <rahul.tamkhane@gmail.com>
2025-02-12 12:55:50 +0530

    Negative number(s) exception added

--- string_calc.dart
+++ string_calc.dart
@@ -4,7 +4,7 @@
 
 void main() {
   // An input string of comma-separated numbers
-  String input = "1\n5,5";
+  String input = "1\n5,5\n2";
   int output = add(input);
   print('Input: $input');
   print('Output: $output');
@@ -17,14 +17,22 @@ int add(String numbers) {
   int sum = 0;
   final re = RegExp(r'[,\n]');
   List<String> nums = numbers.split(re);
+  List<int> negNums = [];
+
   if (nums.length == 0)
     return 0;
   else if (nums.length == 1)
     return int.parse(nums[0]);
   else {
     for (String n in nums) {
-      sum += int.parse(n);
+      int x = int.parse(n);
+      if (x < 0)
+        negNums.add(x);
+      else
+        sum += x;
     }
   }
+  if (negNums.length > 0)
+    throw new Exception('Negative numbers not allowed: ${negNums.join(', ')}');
   return sum;
 }
